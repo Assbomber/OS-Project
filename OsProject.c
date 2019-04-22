8d54fb65a6d72ba27281f78f2b309c1ae723fddf
Aman kumar <36842116+Assbomber@users.noreply.github.com>
2019-04-22 11:11:45 +0530

    Turn around Time

--- OsProject.c
+++ OsProject.c
@@ -2,7 +2,8 @@
 	#include<conio.h>
 	
 	void function1();
-	static int Total_wait_time,Average_wait_time;
+	static int Total_wait_time;
+	static float Average_wait_time;
 	void main()
 	{
 	    function1();
@@ -13,6 +14,7 @@
 	void function1(){
 	    char p[10][5],temp[5];
 	  int i,j,pt[10],wt[10],totwt=0,pr[10],temp1,n;
+	  unsigned pturn[10];
 	  float avgwt;
 	 printf("Please enter the number of processes to create:");
 	 scanf("%d",&n);
@@ -41,15 +43,19 @@
 	             }
 	        }
 	  }
-	 wt[0]=0;
+	  wt[0]=0;
+	  pturn[0]=pt[0]-wt[0];
 	 for(i=1;i<n;i++){
 	        wt[i]=wt[i-1]+pt[i-1];
 	        totwt=totwt+wt[i];
+	        pturn[i]=pt[i]-wt[i];
 	  }   
-	avgwt=(float)totwt/n;   
-	printf("p_name\t p_time\t priority\t w_time\n");
+	avgwt=(float)totwt/n; 
+	
+	
+	printf("Process Name\t Time\t Priority\t Wait Time\tTurnAroundTime\n");
 	for(i=0;i<n;i++){
-	        printf(" %s\t %d\t %d\t\t %d\n" ,p[i],pt[i],pr[i],wt[i]);
+	        printf(" %s\t\t %d\t %d\t\t %d\t\t%d\n" ,p[i],pt[i],pr[i],wt[i],pturn[i]);
 	}
 	Total_wait_time=totwt;
 	Average_wait_time=avgwt;
